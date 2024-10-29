638756f44c9ca4965c409ad15186a9500898a031
Og_Vobla <gggg26705@gmail.com>
2024-10-29 20:21:02 +0300

    Базовый настройки
    Импортирован пакет Alamofire, удалена страница ContentView

--- RickAndMortyWorldWiki/RickAndMortyWorldWikiApp.swift
+++ RickAndMortyWorldWiki/RickAndMortyWorldWikiApp.swift
@@ -10,8 +10,7 @@ import SwiftUI
 @main
 struct RickAndMortyWorldWikiApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        WindowGroup {  
         }
     }
 }
