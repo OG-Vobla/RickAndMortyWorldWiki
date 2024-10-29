11cf4dade71c37de43c31f8a1d395cf7d311b994
Og_Vobla <gggg26705@gmail.com>
2024-10-29 21:45:07 +0300

    Создана главная страница

---
+++ RickAndMortyWorldWiki/Sources/Views/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  MainView.swift
+//  RickAndMortyWorldWiki
+//
+//  Created by Радэль Зубаиров on 29.10.2024.
+//
+
+import SwiftUICore
+
+struct MainView: View {
+    var body: some View {
+        VStack{
+            Image("RickAndMortyTextImage")
+            MenuView()
+        }
+    }
+}
