b9bf00586e3f16e01dcb50dba97c156c8094c623
Og_Vobla <gggg26705@gmail.com>
2024-10-29 10:24:53 +0300

    Initial Commit

---
+++ RickAndMortyWorldWiki/RickAndMortyWorldWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWorldWikiApp.swift
+//  RickAndMortyWorldWiki
+//
+//  Created by Радэль Зубаиров on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWorldWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
