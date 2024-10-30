4b7dcb733333d2d13ba06d62de654a8eff845f8d
Og_Vobla <gggg26705@gmail.com>
2024-10-30 20:01:22 +0300

    Изменена станица CharacterView
    Добавлен вывод персонажей и возможность переходить по страницам

--- RickAndMortyWorldWiki/Sources/Views/CharactersView.swift
+++ RickAndMortyWorldWiki/Sources/Views/CharactersView.swift
@@ -12,19 +12,32 @@ struct CharactersView: View {
     @ObservedObject private var characterListViewModel: CharacterListViewModel = CharacterListViewModel()
     
     var body: some View {
-        ZStack{
+        HStack {
+            Button("<") {
+                characterListViewModel.backPage()
+            }
+            Text("\(characterListViewModel.page)")
+            Button(">") {
+                characterListViewModel.nextPage()
+            }
+        }
+        .frame(alignment: .top)
+        ZStack(alignment: .top) {
             if characterListViewModel.isLoading {
                 ProgressView()
             }
-            
-            if characterListViewModel.characters == nil {
-                Text("No character yet")
-            }
-            
-            ScrollView {
-                LazyVStack {
-                    ForEach(characterListViewModel.characters ?? [], id: \.id) { character in
-                        Text(character.name)
+            else {
+                if characterListViewModel.characters == nil {
+                    Text("No character yet")
+                }
+                else {
+                    ScrollView {
+                        VStack (spacing: 50) {
+                            ForEach(characterListViewModel.characters ?? [], id: \.id) { character in
+                                CharacterView(character: character)
+                            }
+                        }
+                        .padding(10)
                     }
                 }
             }
@@ -32,6 +45,7 @@ struct CharactersView: View {
         .onAppear() {
             characterListViewModel.fetchCharacters()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         
     }
 }
