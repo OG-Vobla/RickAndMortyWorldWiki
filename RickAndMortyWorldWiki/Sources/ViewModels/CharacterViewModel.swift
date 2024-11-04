fd47ad520be930bd9c5d50ddbb223338444378fa
Og_Vobla <gggg26705@gmail.com>
2024-11-04 19:42:12 +0300

    Добавлена логика фильтрации

--- RickAndMortyWorldWiki/Sources/ViewModels/CharacterViewModel.swift
+++ RickAndMortyWorldWiki/Sources/ViewModels/CharacterViewModel.swift
@@ -13,19 +13,25 @@ import SwiftUICore
 public class CharacterListViewModel: ObservableObject {
     @Published var isLoading = true
     @Published var characters: [CharacterModel]? = []
+    @Published var errorMessage: Error?
     public var page: Int = 1
     private var pages: Int = 1
+    var filters: FilterProtocol?
+    var selectedFilters: [Filter] = []
     
-    
-    func fetchCharacters() {
+    func fetchCharacters(){
         self.isLoading = true
-        NetworkManager.shared.fetchCharacters(page: page) { result, error, pages in
-            if let newError = error{
+        NetworkManager.shared.fetchCharacters(page: page, filters: selectedFilters) { result, error, pages in
+            if let newError = error {
                 debugPrint(newError)
                 self.isLoading = false;
+                self.errorMessage = newError
                 return
             }
-            
+            else {
+                self.errorMessage = nil
+            }
+                
             self.characters = result
             
             if let newPages = pages {
@@ -56,5 +62,14 @@ public class CharacterListViewModel: ObservableObject {
         else {
             fetchCharacters()
         }
+    }
+    
+    func filterCharacters(selectedFilter: Filter) {
+        selectedFilters.removeAll { $0.filterName == selectedFilter.filterName }
+        page = 1
+        if selectedFilter.filterSelected != nil {
+            self.selectedFilters.append(selectedFilter)
+        }
+        fetchCharacters()
     }
 }
--- RickAndMortyWorldWiki/Sources/Views/CharactersView.swift
+++ RickAndMortyWorldWiki/Sources/Views/CharactersView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct CharactersView: View {
     @ObservedObject private var characterListViewModel: CharacterListViewModel = CharacterListViewModel()
+    private var filters: [FilterProtocol.Type] = [GenderFilter.self, SpeciesFilter.self, StatusFilter.self]
     
     var body: some View {
         HStack {
@@ -22,30 +23,58 @@ struct CharactersView: View {
             }
         }
         .frame(alignment: .top)
-        ZStack(alignment: .top) {
-            if characterListViewModel.isLoading {
-                ProgressView()
+        VStack {
+            HStack {
+                ForEach((0..<filters.count), id: \.self) {
+                    PickerView(characterListViewModel: characterListViewModel, filterType: filters[$0])
+                }
             }
-            else {
-                if characterListViewModel.characters == nil {
-                    Text("No character yet")
+            ZStack(alignment: .top) {
+                if characterListViewModel.isLoading {
+                    ProgressView()
                 }
                 else {
-                    ScrollView {
-                        VStack (spacing: 50) {
-                            ForEach(characterListViewModel.characters ?? [], id: \.id) { character in
-                                CharacterView(character: character)
+                    if characterListViewModel.characters == nil {
+                        Text("No character yet")
+                    }
+                    else if characterListViewModel.errorMessage != nil {
+                        Text(characterListViewModel.errorMessage!.localizedDescription)
+                    }
+                    else {
+                        ScrollView {
+                            VStack (spacing: 50) {
+                                ForEach(characterListViewModel.characters ?? [], id: \.id) { character in
+                                    CharacterView(character: character)
+                                }
                             }
+                            .padding(10)
                         }
-                        .padding(10)
                     }
                 }
             }
+            .onAppear() {
+               characterListViewModel.fetchCharacters()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            
         }
-        .onAppear() {
-            characterListViewModel.fetchCharacters()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+    public struct PickerView: View {
+        @ObservedObject public var characterListViewModel: CharacterListViewModel
+        @State public var selectedFilter: String? = nil
         
+        var filterType: FilterProtocol.Type
+        var body: some View{ Picker("Select \(filterType.filterName)", selection: $selectedFilter) {
+            Text("All").tag(nil as String?)
+            ForEach(filterType.filterVariants, id: \.self) { filterVariant in
+                Text(filterVariant).tag(filterVariant)
+            }
+        }
+        .pickerStyle(MenuPickerStyle())
+        .onChange(of: selectedFilter) {
+                characterListViewModel.filterCharacters(
+                    selectedFilter: Filter(filterName: filterType.filterName, filterSelected: selectedFilter))
+            }
+        }
     }
 }
